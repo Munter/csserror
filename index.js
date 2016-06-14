4561cc72420b4879a3c65b28e5c20b6b88bf9e3e
Peter Müller <munter@fumle.dk>
2016-06-14 17:24:06 +0200

    Make the css template slightly easier to work with

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var createCssStringLiteral = require('./lib/createCssStringLiteral');
 
 function createCssError(str, doubleEscape) {
-  var tpl = 'body * {\n  display: none !important;\n}\n\nbody:before {\n  line-height: 1.5;\n  display: block;\n  z-index: 99999999;\n  white-space: pre;\n  font-family: "Courier New", monospace;\n  font-size: 20px;\n  color: black;\n  background-color: white;\n  margin: 10px;\n  padding: 10px;\n  border: 4px dashed red;\n  margin-bottom: 10px;\n  content: __ERRORMESSAGE__;\n}\n';
+  var tpl = [
+    'body * {',
+    '  display: none !important;',
+    '}',
+    'body:before {',
+    '  line-height: 1.5;',
+    '  display: block;',
+    '  z-index: 99999999;',
+    '  white-space: pre;',
+    '  font-family: "Courier New", monospace;',
+    '  font-size: 20px;',
+    '  color: black;',
+    '  background-color: white;',
+    '  margin: 10px;',
+    '  padding: 10px;',
+    '  border: 4px dashed red;',
+    '  margin-bottom: 10px;',
+    '  content: __ERRORMESSAGE__;',
+    '}'
+  ].join('\n');
+
+  console.log(tpl);
 
   return tpl.replace('__ERRORMESSAGE__', createCssStringLiteral(str, doubleEscape));
 }
